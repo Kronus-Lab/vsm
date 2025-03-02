c7fc2a896529e066ec864a4e3d2168d2a261941f
Mathieu Dugas <mathieu.dugas@gmail.com>
2025-03-02 00:56:08 -0500

    fixing nitpick comments from Coderabbit

--- test/presetup/wait.sh
+++ test/presetup/wait.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Helper function to wait for a given url/name
+wait_for_service() {
+  local url="$1"
+  local name="$2"
+  echo "Waiting for $name to start"
+  while true; do
+    status=$(curl -sI "$url" | head -n1 | cut -d ' ' -f 2)
+    # Ensure status is numeric; if not, assume the service is not ready
+    if [[ "$status" =~ ^[0-9]+$ ]] && [ "$status" -lt 400 ]; then
+      break
+    fi
+    echo -n '.'
+    sleep 1
+  done
+  echo ""
+}
+
 # Wait for HAProxy to awake
 echo "Giving HAProxy 5 seconds to start"
 sleep 5
 
-# Wait for Keycloak to be up
-echo "Waiting for Keycloak to start"
-while [[ $(curl -sI http://kc.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]]; do
-    echo -n '.'
-    sleep 1
-    done
-
-echo ''
-
-# Wait for Vault to be up
-echo "Waiting for Vault to start"
-while [[ $(curl -sI http://hcv.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]]; do
-    echo -n '.'
-    sleep 1
-    done
++wait_for_service "http://kc.local.kronus.network" "Keycloak"
++wait_for_service "http://hcv.local.kronus.network" "Vault"
