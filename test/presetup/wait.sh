edc3f17188d93a2231e670d08248b1723b3fd4fe
Mathieu Dugas <mathieu.dugas@gmail.com>
2025-03-02 00:11:43 -0500

    Changing wait to use bash and use double square brackets

--- test/presetup/wait.sh
+++ test/presetup/wait.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Wait for HAProxy to awake
 echo "Giving HAProxy 5 seconds to start"
@@ -6,7 +6,7 @@ sleep 5
 
 # Wait for Keycloak to be up
 echo "Waiting for Keycloak to start"
-while [ $(curl -sI http://kc.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]; do
+while [[ $(curl -sI http://kc.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]]; do
     echo -n '.'
     sleep 1
     done
@@ -15,7 +15,7 @@ echo ''
 
 # Wait for Vault to be up
 echo "Waiting for Vault to start"
-while [ $(curl -sI http://hcv.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]; do
+while [[ $(curl -sI http://hcv.local.kronus.network | head -n1 | cut -d ' ' -f 2) -ge 400 ]]; do
     echo -n '.'
     sleep 1
     done
